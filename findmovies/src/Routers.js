e0fdea4ca02a06b8b18bef76cd09546580d348d0
WerbystonLemos <werbyston@hotmail.com>
2022-03-08 22:08:14 -0300

    conclusao do front

--- findmovies/src/Routers.js
+++ findmovies/src/Routers.js
@@ -8,7 +8,7 @@ const  Routes = () =>
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/movie" component={Movie} />
+                <Route path="/movie" component={Movie} />
             </Switch>
         </BrowserRouter>
     )
